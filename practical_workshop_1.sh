706f6c98e40e1f69f5bd5e8a5826bf44ed76faec
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-03-23 12:58:52 +0000

    Listprivatekeyspartner

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -10,3 +10,10 @@ gpg --armor --export yumbilloariel@gmail.com > mi_llave_publica.asc
 gpg --list-secret-keys --keyid-format=long
 #If we have some keys in our list, we need specifie the  hash, it means, we need to use the sec
 gpg --armor --export-secret-keys 70571C1279AE9727
+#We can  list our pair of keys and we execute:
+gpg --armor --export-secret-keys
+#In this moment, we are wortking in pairs and the first step is list our keys
+gpg --list-keys
+#We need download the file (mypartner)
+#Then use to import andres
+gpg --import andres_llave_publica.asc
