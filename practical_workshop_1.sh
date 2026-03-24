d627e7d82896f4b6043d85a119df9024f5b857f8
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-03-24 23:50:08 +0000

    Bloqshomework

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -1,3 +1,4 @@
+#BLOQ A
 uname -a
 which gpg 
 gpg --version
@@ -5,6 +6,7 @@ gpg --version
 #RSA 4096, son bits universales
 gpg --full-generate-key
 gpg --list-keys
+#BLOQ B
 gpg --armor --export yumbilloariel@gmail.com > mi_llave_publica.asc
 #list private keys
 gpg --list-secret-keys --keyid-format=long
@@ -19,6 +21,7 @@ gpg --list-keys
 gpg --import andres_llave_publica.asc
 #If all its okey we can use:
 gpg --list-keys
+#BLOQ C
 #echo = We use echo to show in console
 #>= It is the filter to txt
  echo "hola andres" >doc_no_cifrado.txt
@@ -27,11 +30,15 @@ gpg --list-keys
 do.txt
 #My partner sent me the file. so I need descrypt the file and with the code, it shows the message
  $ gpg --decrypt holaarieltilin.txt
+ #BLOQ D
 #Create a new file with our sign 
  gpg --output docfirmado.txt --clearsign doc_no_cifrado.txt
+ 
 #Verify the sign
 gpg --verify doc_no_cifrado_firmado.txt
 #We can edit the key and tyhe confidence
  gpg --edit-key 10764728C8B9C7F941FCF5F49CE2A432BF706712
  # Do the file in binary
  gpg --output doc_no_cifrado_firmado_binario.txt --sign doc_no_cifrado.txt
+ 
+ 
