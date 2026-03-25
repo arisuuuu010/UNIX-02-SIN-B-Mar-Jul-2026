e7bf3cce70720f29f10954aecf299f01cb3f8fd8
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-03-25 13:11:48 +0000

    SALUDO.C PRACTICE

---
+++ SALUDO.C
@@ -0,0 +1,11 @@
+//start the code with a library
+#include <stdio.h>
+// C is a typed language so, when we programmin here, we must specify the int
+// Main function returns an integer to the OS
+int main() {
+    printf("Hello word!/n");
+    // Return 0 means the program finished successfully
+    return 0;
+
+}
+//pwd (print work directory)
