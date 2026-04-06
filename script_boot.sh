56c130780c3fe642d0d6327d36e9e71976cb1b05
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-06 12:34:09 +0000

    Absolute way and relative way

--- script_boot.sh
+++ script_boot.sh
@@ -2,4 +2,5 @@
 # cd (Change directory, when we want change the directory we use cd and the name of the place)
 #Example: cd /home/codespace
 # cd ~ (Is a shortcut used to change your current working directory to your Home directory)
-# cd $HOME (Is used to navigate directly to your Home Directory from any location in the file system)
+#cd $HOME  
+#but $HOME is wrong beacuse we cant use $ if we want change the directory
