92ca8ddee921232f889723738e790c10a3ae64ba
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-06 12:30:29 +0000

    Absolute way and relative way

---
+++ script_boot.sh
@@ -0,0 +1,5 @@
+## / (Main directory, it helps to know the place that we are)
+# cd (Change directory, when we want change the directory we use cd and the name of the place)
+#Example: cd /home/codespace
+# cd ~ (Is a shortcut used to change your current working directory to your Home directory)
+# cd $HOME (Is used to navigate directly to your Home Directory from any location in the file system)
