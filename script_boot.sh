31476b73c358b97fa07bece0d36769f492f068be
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-06 12:41:32 +0000

    create proyect file

--- script_boot.sh
+++ script_boot.sh
@@ -4,3 +4,8 @@
 # cd ~ (Is a shortcut used to change your current working directory to your Home directory)
 #cd $HOME  
 #but $HOME is wrong beacuse we cant use $ if we want change the directory
+#mkdir (we can create a file in the terminal, using mkdir and if want romove a file, we use rmdir)
+# ls -lai (List files, but give us hide information)
+total 12 #(Here we have . and .., the are hidden files and when we use lai, we can wacth it)
+925555 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:35 . #(The numbers are the inodos, it means the ids of each file)
+918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:35 ..
