3dae04af470ecff59baefa3869fe57c71fbd7217
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-13 12:42:42 +0000

    ls -l -a -h information

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
 
 The structure that we have in a linux command are, command, options and arguments
-ls -a (Short command)
-ls --all (Large command)
+ls -a (Short command, if we want a fast typing (hacking NASA)) 
+ls --all (Large command, the better use is when we reading scripts) 
 In both cases are similar, the mean of each one is give me all the files
 also the structure in both commands are, command and option.
+
+ls -l -a -h (This command help us wehn we know all the files with every information of each one, it means, hidden files, dates and who is the propietarie.)
+ls -l -ah (With this commmand we have the same information)
+ls -lah (-lah is better when we want a fast typing (Hacking FBI))
