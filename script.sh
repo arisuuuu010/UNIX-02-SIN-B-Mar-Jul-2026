625892b6d15473504cd002ae590bf02be98b3ab4
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-13 12:33:04 +0000

    ls-a information

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
-ls -a
-ls --all
+
 The structure that we have in a linux command are, command, options and arguments
+ls -a (Short command)
+ls --all (Large command)
+In both cases are similar, the mean of each one is give me all the files
+also the structure in both commands are, command and option.
