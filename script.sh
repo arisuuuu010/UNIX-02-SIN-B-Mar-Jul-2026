93565daa771f05c71ee0c0edd08296de286b9ff5
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-15 12:39:24 +0000

    Sudo

--- script.sh
+++ script.sh
@@ -30,6 +30,13 @@ man git-clone (Clones a repository into a newly created directory, creates remot
 
 chmod +x script.sh (All the people can execute the script)
 chmod u+x script.sh (U = users and x = execute, said, only users can execute the script)
-chmod o-r script.sh (O= others and -=less and r = read, it means others cannot read the script)
+chmod o-r script.sh (O= others and - =less and r = read, it means others cannot read the script)
 chmod u+rw,go-rwx script.sh (u = users + = can  r = read w = write, go = group and others - = less r =read w = write and x =execure)
 it means Users can read and write the script, in the other hand, group and others cannot read, write and execute the script.
+
+sudo echo "hola" > /etc/archivo_protegido (Here we have a problem like, the frist commmand part has sudo, it means, all the athorized permissions , in the other hand, the other command after > doesnt have permissions)
+echo "hola" | sudo tee /etc/archivo_protegido > /dev/null (This command its netter than use  the redirection >, because we can executre this command with a pipeline and the sudo must be for all the command, however/dev/null, dont let see the content)
+cat /etc/archivo_protegido(We can use cat to see the file content)
+echo "hola" | sudo tee /etc/archivo_protegido (We have the same, however /dev/null are eliminated, and the command give the content wehn we executre it)
+
+
