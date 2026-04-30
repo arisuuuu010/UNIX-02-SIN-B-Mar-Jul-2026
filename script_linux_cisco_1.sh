4cf072ef2b34a879a4ef10d6143882560c4f1221
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-30 20:40:28 +0000

    first command, ls basic information

---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,9 @@
+Cisco Homework
+1.ls //ls is a basic command in linux, ls list files and directories in the computer
+we have some examples add to ls, like, ls -h, ls -a, ls -lah, each one give more information than single file.
+When I read the mean of ls in the course, a fun fact that I find out is, linux has case sensitive, it means depends on if the 
+command has uppercase or not and if the command has space or not.
+Also commands get structure:
+1.command
+2.options
+3.arguments
