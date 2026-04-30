0a99c7390fcb2b6a322d8a99a9e0b72755caf05d
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-30 21:30:04 +0000

    third command aptitude

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -9,3 +9,4 @@ Also commands get structure:
 3.arguments
 2.ls Documents //Actually when we talk about command structure, we have a good example like ls Documents, the function is give all
 the documents that you have in your computer.
+3.aptitude moo command is a multi-layered Easter egg found in the Aptitude package manager on Debian-based Linux distributions
