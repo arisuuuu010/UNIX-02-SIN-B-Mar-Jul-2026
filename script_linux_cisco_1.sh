96e109c66937acd34d73c6044d452f9e7c86b858
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-30 22:02:59 +0000

    sixth command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -10,9 +10,11 @@ Also commands get structure:
 2.ls Documents //Actually when we talk about command structure, we have a good example like ls Documents, the function is give all
 the documents that you have in your computer.
 3.aptitude moo command is a multi-layered Easter egg found in the Aptitude package manager on Debian-based Linux distributions
+if you want play with the command you can use, -v, example, aptitude -v moo
 4.ls -l // If you want more information in a large screen you can use ls -l, you get information like, permissions. dates, etc.
 5.ls -r //When you run a standard ls command, the system displays your files and folders in alphabetical order. The -r flag tells the terminal to flip that list upside down, sorting from Z to A instead of A to Z.
 ls -l -r
 ls -rl
 ls -lr
-//they have the same function as ls -r with more information
+//they have the same function as ls -r with more information.
+6.pwd //When you type this into your terminal and hit Enter, it tells you exactly where you are currently located in the file system hierarchy.
