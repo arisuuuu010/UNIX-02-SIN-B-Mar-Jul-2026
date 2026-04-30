f8e73a5a1dc66ed0652e375198f0b60e8759b032
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-04-30 21:47:04 +0000

    fifth command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -11,3 +11,8 @@ Also commands get structure:
 the documents that you have in your computer.
 3.aptitude moo command is a multi-layered Easter egg found in the Aptitude package manager on Debian-based Linux distributions
 4.ls -l // If you want more information in a large screen you can use ls -l, you get information like, permissions. dates, etc.
+5.ls -r //When you run a standard ls command, the system displays your files and folders in alphabetical order. The -r flag tells the terminal to flip that list upside down, sorting from Z to A instead of A to Z.
+ls -l -r
+ls -rl
+ls -lr
+//they have the same function as ls -r with more information
