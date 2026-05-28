94b62e1c5e6657c20c3c226bcf16cc09e1a8df23
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-05-28 02:32:22 +0000

    newgrp use

--- security2.sh
+++ security2.sh
@@ -3,4 +3,11 @@ id -gn #It can show the name of the main group
 #Create a file and see which group inherits
 touch ~/test_grupo_heredado.txt 
 ls -la ~/test_grupo_heredado.txt
-#The group is the main group of the user
+#The group is the main group of the user.
+
+#See the group in my case vscode
+echo "Grupo actual: $(id -gn)" 
+#Create a new file before use of newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
+
