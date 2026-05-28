4369b1d4f137aba0c712bbd90078cec76735c679
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-05-28 02:54:16 +0000

    change group

--- security2.sh
+++ security2.sh
@@ -12,4 +12,10 @@ touch ~/antes_de_newgrp.txt
 ls -la ~/antes_de_newgrp.txt
 
 newgrp #In my case i need to install the dependencies about newgrp
+#@arisuuuu010 ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (security2) $ newgrp desarrolladores
+#newgrp: group 'desarrolladores' does not exist
 
+id -gn
+#@arisuuuu010 ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (security2) $ echo "Nuevo grupo activo $(id -gn)"
+#Nuevo grupo activo desarrolladores
+# It shows how can we change the group
