fa0aa2e75b9ae578857c21cf1fc582f10495179d
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-05-28 02:29:17 +0000

    create test grupo

--- security2.sh
+++ security2.sh
@@ -1,2 +1,6 @@
 id 
 id -gn #It can show the name of the main group
+#Create a file and see which group inherits
+touch ~/test_grupo_heredado.txt 
+ls -la ~/test_grupo_heredado.txt
+#The group is the main group of the user
