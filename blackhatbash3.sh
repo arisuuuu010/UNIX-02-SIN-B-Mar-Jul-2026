2c070d38586f403b86c9c8c34ebe12bed7661f5e
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-06-01 13:40:35 +0000

    more infoamtion

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -3,3 +3,7 @@ book="black hat bash"
 echo "This book's name is ${book}" 
 echo "This book's name is $book"
 # bash -x blackhatbash3.sh (verbose moode)
+
+
+ root_directory=$(ls -ld /)
+ echo "${root_directory}"
