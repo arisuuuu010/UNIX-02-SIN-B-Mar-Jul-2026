48ab5f29f20754fa085caa95e088bb3a16fbcdab
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-06-04 13:49:38 +0000

    files unodos

--- blackhastbash5.sh
+++ blackhastbash5.sh
@@ -3,3 +3,10 @@ touch test && touch123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "El comando lzl fallo"
+
+echo "uno" > file.txt
+echo "dos" > file.txt
+echo "uno-uno" >> file.txt
+
+echo "3" &> file1.txt
+echo "3" &>> file1.txt
