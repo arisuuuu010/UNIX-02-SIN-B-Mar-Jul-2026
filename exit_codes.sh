01f39a8081e30cfd6d6c13f0e2f05047bf54da84
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-06-08 13:24:19 +0000

    exit code

--- exit_codes.sh
+++ exit_codes.sh
@@ -4,3 +4,9 @@ ls -l > /dev/null
 echo "The status code of the ls command was: $?"
 lzl 2> /dev/null
 echo "The status code of the non-existing lzl command was: $?"
+
+echo "Exiting wiith exit code: 223"
+exit 223
+
+ps -ef
+echo $?
