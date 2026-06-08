e0ae6d32ca4229d9cd99efdbcd1362e8f6bf292f
arisuuuu010 <aryumbillopa@uide.edu.ec>
2026-06-08 12:57:14 +0000

    we can executre this with more ips

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -8,3 +8,8 @@ ping -c 3 "${TARGET}"
 
 echo "The arguments are: $@"
 echo "The total number of arguments are: $#"
+
+# Change "$@" to "$*" to observe behavior.
+for args in "$@"; do
+echo "${args}"
+done
